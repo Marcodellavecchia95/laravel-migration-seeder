9fd8e63b8942790c7b5e8505e556875ead189ba2
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-08-15 14:10:43 +0200

    add trains to homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,7 @@
+<?php
+$today = date("Y-m-d");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +15,30 @@
 <body>
     <div class="container">
         <h1 class="text-center my-3">Treni in partenza oggi</h1>
+        <div class="row g-3">
+            @foreach($trains as $train)
+            @if($train->departure_date >= $today)
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+
+                    <div class="card-body">
+                        <h5 class="card-title">{{$train->company}} {{$train->train_code}} </h5>
+                        <p class="card-text">Diretto a {{$train->arrival_station}}</p>
+                        <p class="card-text">Partirà alle {{$train->departure_time}}</p>
+                        @if($train->in_time)
+                        <p class="card-text">IN ORARIO</p>
+                        @else
+                        <p class="card-text">IN RITARDO</p>
+                        @endif
+                        @if($train->is_deleted)
+                        <p class="card-text">CANCELLATO</p>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            @endif
+            @endforeach
+        </div>
     </div>
 </body>
 
