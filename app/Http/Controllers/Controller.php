62a714df331b3407688f7c1dc95ece92ee2a3c38
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-08-15 13:55:10 +0200

    create column departure_date

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,14 +2,4 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Train;
-
-class Controller
-{
-    public function index()
-    {
-        $trains = Train::all();
-
-        return view("index", compact("trains"));
-    }
-}
+abstract class Controller {}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trains</title>
+    @vite(["resources/sass/app.scss"])
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center my-3">Treni in partenza oggi</h1>
+    </div>
+</body>
+
+</html>
